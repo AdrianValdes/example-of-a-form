8fcf96c76ba095bdca9b56e33c43d57ec38f825d
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-03-27 15:49:46 +0100

    Updates

---
+++ precache-manifest.fa4a7230d52d496908d7097039226bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c37f0214126c6790b2b752ab3526ff",
+    "url": "/example-of-a-form/index.html"
+  },
+  {
+    "revision": "a4a3f1eb897a5c174d2f",
+    "url": "/example-of-a-form/static/css/main.cba603e5.chunk.css"
+  },
+  {
+    "revision": "4331e36fbfbd5cf84ee7",
+    "url": "/example-of-a-form/static/js/2.b513cc7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example-of-a-form/static/js/2.b513cc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a3f1eb897a5c174d2f",
+    "url": "/example-of-a-form/static/js/main.e660cb6a.chunk.js"
+  },
+  {
+    "revision": "aa634258e54599c045c8",
+    "url": "/example-of-a-form/static/js/runtime-main.257fc689.js"
+  }
+]);
